132fe1e15c1b23217a85173a4448cee47f0e616f
Ivan Ivanov <78234071+ivanivanov10101@users.noreply.github.com>
2023-05-22 19:23:44 +0200

    up to 34:17 at tutorial

---
+++ youtube-clone/src/components/SearchBar.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import { useNavigate} from "react-router-dom";
+
+import { Paper, IconButton } from "@mui/material";
+import { SearchIcon } from '@mui/icons-material';
+
+const SearchBar = () => (
+    <Paper
+        component="form"
+        onSubmit={() => {}}
+        sx={{
+            borderRadius: 20,
+            border: '1px solid #e3e3e3',
+            pl: 2,
+            boxShadow: 'none',
+            mr: { sm: 5 }}>
+            </Paper>
+
+)
+
+export default SearchBar;
