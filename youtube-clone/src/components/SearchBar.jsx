961c261419926ac41ba458fdf3339b5869dbd95e
Ivan Ivanov <78234071+ivanivanov10101@users.noreply.github.com>
2023-05-28 18:41:31 +0200

    part 2 @ 58:00

--- youtube-clone/src/components/SearchBar.jsx
+++ youtube-clone/src/components/SearchBar.jsx
@@ -2,19 +2,31 @@ import { useState } from 'react';
 import { useNavigate} from "react-router-dom";
 
 import { Paper, IconButton } from "@mui/material";
-import { SearchIcon } from '@mui/icons-material';
+import { Search } from '@mui/icons-material';
+import React from ".";
 
 const SearchBar = () => (
     <Paper
-        component="form"
+        component='form'
         onSubmit={() => {}}
         sx={{
             borderRadius: 20,
             border: '1px solid #e3e3e3',
             pl: 2,
             boxShadow: 'none',
-            mr: { sm: 5 }}>
-            </Paper>
+            mr: { sm: 5 },
+        }}
+    >
+        <input
+            className='search-bar'
+            placeholder='Search...'
+            value=""
+            onChange={() => {}}
+        />
+        <IconButton type='submit' sx={{ p: '10px', color: 'red' }} aria-label='search'>
+            <Search />
+        </IconButton>
+    </Paper>
 
 )
 
