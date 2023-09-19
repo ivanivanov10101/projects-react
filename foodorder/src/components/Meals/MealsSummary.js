f2bc66a8c5acc88c0b25261eb9bd86b0560812b9
Ivan Ivanov <78234071+ivanivanov10101@users.noreply.github.com>
2023-09-19 15:55:11 +0200

    upload old foodorder site project

---
+++ foodorder/src/components/Meals/MealsSummary.js
@@ -0,0 +1,18 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Bestel eten en meer</h2>
+      <p>
+        Restaurants en boodschappen in de buurt
+      </p>
+      <p>
+        Zo werkt het
+        Makkelijker kan haast niet.
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
