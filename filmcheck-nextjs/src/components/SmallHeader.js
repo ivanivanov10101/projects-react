f96996f1530f7577e16e2a8edcca87430954d9fa
Ivan Ivanov <78234071+ivanivanov10101@users.noreply.github.com>
2023-10-17 19:09:03 +0200

    films page fixed

--- filmcheck-nextjs/src/components/SmallHeader.js
+++ filmcheck-nextjs/src/components/SmallHeader.js
@@ -1,21 +1,17 @@
-import {useState} from "react";
-import { LazyLoadImage } from "react-lazy-load-image-component";
-import { useNavigate } from "react-router-dom";
+import Link from "next/link";
+import Image from "next/image";
 
-const SmallHeader = ({heading, paragraph, children, image}) => {
-  const [state] = useState({
+const SmallHeader = ({image}) => {
+  const state = {
     poster: './assets/images/img.avif',
     logo: './assets/images/logo.png'
-  })
-  const navigate = useNavigate()
-  const goToHome = () =>{
-    navigate('/');
   }
+
   return(
     <div className='small-header'>
       <div className='container'>
         <div className='small-header__logo'>
-          <LazyLoadImage src={state.logo} alt="logo-image" onClick={goToHome}/>
+          <Link href="/"><Image src={"/logo.png"} width={100} height={100} alt="logo-image" /></Link>
         </div>
       </div>
       <div className='header__image'>
